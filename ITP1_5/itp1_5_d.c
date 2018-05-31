5e758480fbbc4ff2e8e256f6c2633c70230ecba5
s1260188 <s1260188@u-aizu.ac.jp>
2018-05-31 15:44:32 +0900

    first commit

---
+++ ITP1_5/itp1_5_d.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+
+int main(){
+   int n,i,x;
+   scanf("%d",&n);
+   for(i=1;i<=n;i++){
+     x=i;
+     if(x%3==0){
+       printf(" %d",i);
+     }else if(x%10==3){
+       printf(" %d",i);
+   }else{
+     while(x !=0){
+       x /=10;
+       if(x%10==3){
+         printf(" %d",i);
+         break;
+       }
+     }
+   }
+ }
+   printf("\n");
+   return 0;
+}
